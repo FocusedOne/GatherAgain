aff34eea45da620e78a7374f8a1671f1ff8dc040
FocusedOne <FocusedOne@users.noreply.github.com>
2020-07-22 17:43:45 -0500

    External JS file for service times
    Makes it easier to read / edit service times

---
+++ servicetimes.js
@@ -0,0 +1,23 @@
+
+
+// West Little Rock registration links
+  var wlr9am = document.getElementById('wlr9am');
+  wlr9am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64353"
+  var wlr11am = document.getElementById('wlr11am');
+  wlr11am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64359"
+
+// Benton Registration Links
+  //var benton9am = document.getElementById('benton9am');
+  //benton9am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64425"
+  var benton11am = document.getElementById('benton11am');
+  benton11am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64426"
+
+//Cabot registration links
+  var link = document.getElementById('cabot9am');
+  link.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64436"
+  //var link = document.getElementById('cabot11am');
+  //link.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64402"
+
+//Midtown registration link
+  var link = document.getElementById('mid5pm');
+  link.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64433"
