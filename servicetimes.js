74edaa18193c662d246748773b7538b6a4146502
FocusedOne <FocusedOne@users.noreply.github.com>
2020-07-23 16:01:40 -0500

    Spelling and poster fix
    Fixed spelling, added poster png for video.

--- servicetimes.js
+++ servicetimes.js
@@ -8,22 +8,22 @@ Only uncomment a line here if the corresponding button is uncommented on the htm
 
 // West Little Rock registration links
   var wlr9am = document.getElementById('wlr9am');
-  wlr9am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64353"
+  wlr9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64353"
   var wlr11am = document.getElementById('wlr11am');
-  wlr11am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64359"
+  wlr11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64359"
 
 // Benton Registration Links
   //var benton9am = document.getElementById('benton9am');
-  //benton9am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64425"
+  //benton9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64425"
   var benton11am = document.getElementById('benton11am');
-  benton11am.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64426"
+  benton11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64426"
 
 //Cabot registration links
   var link = document.getElementById('cabot9am');
-  link.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64436"
+  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64436"
   //var link = document.getElementById('cabot11am');
-  //link.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64402"
+  //link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64402"
 
 //Midtown registration link
   var link = document.getElementById('mid5pm');
-  link.href = "https://my.fellowshiponline.com/portal/event_signup.aspx?id=64433"
+  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64433"
