d2e600593696afd05572c91b7ae9ab4dc6637dff
FocusedOne <FocusedOne@users.noreply.github.com>
2020-08-31 10:15:32 -0500

    Fixed servicetimes.js

--- servicetimes.js
+++ servicetimes.js
@@ -8,40 +8,23 @@ Only uncomment a line here if the corresponding button is uncommented on the htm
 
 // West Little Rock registration links
   var wlr9am = document.getElementById('wlr9am');
-<<<<<<< HEAD
-  wlr9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65433"
-  var wlr11am = document.getElementById('wlr11am');
-  wlr11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65434"
-=======
   wlr9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65882"
   var wlr11am = document.getElementById('wlr11am');
   wlr11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65883"
->>>>>>> 7a70acf91828ee2bf6c40a0cc2931e15f799b302
+
 
 // Benton Registration Links
   //var benton9am = document.getElementById('benton9am');
   //benton9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64425"
   var benton11am = document.getElementById('benton11am');
-<<<<<<< HEAD
-  benton11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65430"
-
-//Cabot registration links
-  var link = document.getElementById('cabot9am');
-  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65431"
-=======
   benton11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65874"
 
 //Cabot registration links
   var link = document.getElementById('cabot9am');
   link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65876"
->>>>>>> 7a70acf91828ee2bf6c40a0cc2931e15f799b302
   //var link = document.getElementById('cabot11am');
   //link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=64402"
 
 //Midtown registration link
   var link = document.getElementById('mid5pm');
-<<<<<<< HEAD
-  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65432"
-=======
   link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=65879"
->>>>>>> 7a70acf91828ee2bf6c40a0cc2931e15f799b302
