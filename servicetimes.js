6c4483c143affb469a1761c13e236dc83d160b5d
FocusedOne <FocusedOne@users.noreply.github.com>
2021-02-17 09:24:04 -0600

    210217 Update

--- servicetimes.js
+++ servicetimes.js
@@ -8,23 +8,23 @@ Only uncomment a line here if the corresponding button is uncommented on the htm
 
 // West Little Rock registration links
   var wlr9am = document.getElementById('wlr9am');
-  wlr9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=67382"
+  wlr9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=68375"
   var wlr11am = document.getElementById('wlr11am');
-  wlr11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=67383"
+  wlr11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=68376"
 
 
 // Benton registration links
   var benton9am = document.getElementById('benton9am');
-  benton9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=67381"
+  benton9am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=68365"
   var benton11am = document.getElementById('benton11am');
-  benton11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=67386"
+  benton11am.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=68366"
 
 //Cabot registration links
   var link = document.getElementById('cabot9am');
-  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=67373"
+  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=68369"
   var link = document.getElementById('cabot11am');
-  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=67375"
+  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=68370"
 
 //Midtown registration link
   var link = document.getElementById('mid5pm');
-  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=67371"
+  link.href = "https://my.fellowshiponline.com/portal/event_detail.aspx?id=68372"
